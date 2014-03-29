d1c3a64e3bbd1d383a6221ba0d9f97a1db4c74d1
ruikye <kang@ruikye-2.local>
2014-03-29 15:02:10 +0800

    Initial Commit

---
+++ BReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BReader
+//
+//  Created by ruikye on 14-3-29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
